48c8d8b0f1547885ae8120bdee0b08cc552aa329
mewben <melvinsoldia@gmail.com>
2017-04-10 10:57:09 +0800

    rfc

--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { string, bool, any } from 'prop-types'
 import { Field } from 'react-redux-form/immutable'
 import errBlock from 'bisu-react-form-error'
 
-class BisuReactInput extends Component {
-
-  render() {
-    const { model, type, label, required, ...props } = this.props
-    return (
-      <Field model={model} className="bisu--react-input">
-        <label htmlFor={model}>{label} {required && <span className="req">*</span>}</label>
-        <input
-          type={type}
-          className="form-control"
-          {...props}
-        />
-        {errBlock(model)}
-      </Field>
-    )
-  }
-}
+const BisuReactInput = ({ model, type, label, required, ...props }) => (
+  <Field model={model} className="bisu--react-input">
+    <label htmlFor={model}>{label} {required && <span className="req">*</span>}</label>
+    <input
+      type={type}
+      className="form-control"
+      {...props}
+    />
+    {errBlock(model)}
+  </Field>
+)
 
 BisuReactInput.propTypes = {
   model: string,
